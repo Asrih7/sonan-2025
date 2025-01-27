7a00aa3e9010d3d4e67e57d8c9ba0fd54d3b8d92
unknown <asrihsoufiane@gmail.com>
2025-01-27 19:35:54 +0000

    add work of zakaria

--- src/app/home/adhkar/adhkar.component.ts
+++ src/app/home/adhkar/adhkar.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { selectAdhkarState } from 'src/app/shared/reducer/reducer';
 @Component({
   selector: 'app-adhkar',
   templateUrl: './adhkar.component.html',
   styleUrls: ['./adhkar.component.scss'],
 })
 export class AdhkarComponent  implements OnInit {
+  adkhar$: Observable<any> | undefined;
+  adkharData: any[] | undefined;
+  index: any;
+  constructor(private store: Store) {}
 
-  constructor() { }
-
-  ngOnInit() {}
+ngOnInit() {
+  this.adkhar$ = this.store.pipe(select(selectAdhkarState.dataSelector));
+  this.adkhar$.subscribe(adkharData => {
+    console.log('adhkar from NgRx Store:', adkharData); 
+    this.adkharData = adkharData;
+  });
+}
 
 }
--- src/app/home/adhkar/adhkar.module.ts
+++ src/app/home/adhkar/adhkar.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdhkarRoutingModule } from './adhkar-routing.module';
 import { AdhkarComponent } from './adhkar.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AdhkarComponent],
   imports: [
     CommonModule,
     AdhkarRoutingModule,
+    IonicModule
   ],
 })
 export class AdhkarModule {}
